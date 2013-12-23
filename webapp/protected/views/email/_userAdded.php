f3bef477980291f435035558e01d1d1cd94f144f
keith Gonzales <keith@stirplate.io>
2013-12-23 17:59:01 -0500

    wording changes

--- webapp/protected/views/email/_userAdded.php
+++ webapp/protected/views/email/_userAdded.php
@@ -1,7 +1,6 @@
-You were added to the following study:
-
-<?php echo $studyName ?>
-
-To access this study, go to:
-
-<?php echo $studyUrl ?>
+Hi there science friend,
+<?php echo $user->name;?> has invited you to join the <?php echo $studyName ?>
+Project.
+To access this study, go to <?php echo $studyUrl ?>.
+Thanks!
+The Stirplate team
