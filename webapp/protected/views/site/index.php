d95ac89471cd54ea7a3e821ca4186e39fd6ebcae
munawer-t <munawer.t@gmail.com>
2014-01-07 21:01:25 +0530

    newsletter head color change

--- webapp/protected/views/site/index.php
+++ webapp/protected/views/site/index.php
@@ -525,7 +525,7 @@
                     'id'=>'newsletter-form',
                     'action'=> $this->createUrl('site/newsletter'),
                 )); ?>
-          <h3 align = "center">Stirplate Newsletter, sign up for updates.</h3>
+          <h3 align = "center" class="newsletter-head">Stirplate Newsletter, sign up for updates.</h3>
           <div class="signUpMain">
               <?php echo $form->error($newsLetterModel,'email'); ?>
               <?php echo Yii::app()->user->getFlash('success'); ?>
