c96228cb61b804a7945af636d0ed656221f5d8fb
Mahmoud Abdelaziz <hodafokher@gmail.com>
2014-01-10 01:16:26 +0200

    limit comments string length

--- webapp/protected/views/activity/_activity_streams.php
+++ webapp/protected/views/activity/_activity_streams.php
@@ -4,9 +4,11 @@
   <a href="/project/index/<?= $activity->projectId?>">
     
     <div class="listRt" style="width: 95%;">
-      <?php if ($activity->type == "comment") { ?>
+      <?php if ($activity->type == "comment") { 
+        $more_comment = strlen($activity->content) > 70 ? "..." : "";
+        ?>
         <h6><?= $activity->user->firstName ?></h6>
-        <p>added a new coment "<?= $activity->content ?>" to <?= $activity->project->title ?></p>
+        <p>added a new coment "<?= substr($activity->content, 0, 70).$more_comment ?>" to <?= $activity->project->title ?></p>
       <?php }else if ($activity->type == "user_added"){?>
         <h6><?= $activity->inviter_user->firstName ?></h6>
         <p> added <?=$activity->user->firstName?> to "<?= $activity->project->title ?>"</p>
