fff1de93f798ef5f7f7144fb0db1c7db35f6ca10
Varun <gautamv16@gmail.com>
2014-01-22 00:39:26 +0530

    updating  mxpanel integration

--- webapp/protected/views/layouts/main.php
+++ webapp/protected/views/layouts/main.php
@@ -42,27 +42,28 @@
         </script>
  <?php 
  
- 
+  
   ?>       	
 <script type="text/javascript">(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===e.location.protocol?"https:":"http:")+'//cdn.mxpnl.com/libs/mixpanel-2.2.min.js';f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f);b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==
 typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");for(g=0;g<i.length;g++)f(c,i[g]);
 b._i.push([a,e,d])};b.__SV=1.2}})(document,window.mixpanel||[]);
 mixpanel.init("c37fd07f69d3adb2bfd563af7744e7a6");
 <?php if(isset(Yii::app()->session['event']) && !empty(Yii::app()->session['event'])){
-    $userData=User::model()->findByPk(Yii::app()->session['uid']);
- 
+   $userData=User::model()->findByPk(Yii::app()->session['uid']);
+   
     foreach(Yii::app()->session['event'] as $data)
     {
     ?>
 mixpanel.identify('<?php echo $data['initiator']; ?>');
 mixpanel.people.set({
-    $email: '<?php echo $data['initiator']; ?>',
-    
-    $name: '<?php echo $data['initiator']; ?>'
+    '$email': '<?php echo $data['initiator']; ?>',
+    'First name': '<?php echo $userData->firstName; ?>',
+    'Last Name': '<?php echo $userData->lastName; ?>',
+  
 });    
 mixpanel.track(
     '<?php echo $data['activity']; ?>',
-    { 'Initiator': '<?php echo $userData['first_name'].' '.$userData['last_name'];?>',
+    { 'Initiator': '<?php //echo $userData['first_name'].' '.$userData['last_name'];?>',
        'Description': '<?php echo $data['description']; ?>' 
      });
 <?php }
