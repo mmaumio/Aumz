1ba763d6b5f33d557ca3fd9dc5f237b975240923
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2014-01-30 02:24:21 +0600

    Changes made on the pricing and style.css page

--- webapp/protected/views/site/pricing.php
+++ webapp/protected/views/site/pricing.php
@@ -1,5 +1,25 @@
-<div class="jumbotron">
-  <h1>Hello, world!</h1>
-  <p>...</p>
-  <p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+<div class="container">
+	<div class="jumbotron">
+	  <h1>I am interested in plans for:</h1>
+	  	<ul id="pricing-plan">
+		  	<li class="dropdown">
+		  		<a data-toggle="dropdown" href="#">Myself</a>
+		  			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+		    			nsaisasxn
+		  			</ul>
+			</li>
+			<li class="dropdown">
+		  		<a data-toggle="dropdown" href="#">My lab</a>
+		  			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+		    			cpojksjanka 
+		  			</ul>
+			</li>
+			<li class="dropdown">
+		  		<a data-toggle="dropdown" href="#">I’d like to see both</a>
+		  			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+		    			cpojksjanka 
+		  			</ul>
+			</li>
+		</ul>
+	</div>
 </div>
