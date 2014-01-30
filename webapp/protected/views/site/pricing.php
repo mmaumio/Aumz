4072db15a2e52802b262bc3f6258387e88458d48
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2014-01-31 03:05:03 +0600

    Changes on Myself Tabs Content when clicked

--- webapp/protected/views/site/pricing.php
+++ webapp/protected/views/site/pricing.php
@@ -4,8 +4,141 @@
 	  	<ul id="pricing-plan">
 		  	<li class="dropdown">
 		  		<a data-toggle="dropdown" href="#">Myself</a>
-		  			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-		    			nsaisasxn
+		  			<ul class="dropdown-menu" role="menu" id="myself-tab" aria-labelledby="dLabel">
+		    			<table border="1" width="900">
+							<p align="center">Individual Plan</p>
+							<tr>
+								<td><b>Price</b></td>
+								<td>Free</td>
+								<td>5$ a month</td>
+								<td>20$ a month</td>
+								<td>50$ a month</td>
+							</tr>
+							<tr>
+								<td><b>Base features</b></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Storage</td>
+								<td>2GB</td>
+								<td>10GB</td>
+								<td>2GB</td>
+								<td>100GB</td>
+							</tr>
+							<tr>
+								<td>AES-256 bit  server side encryption ?</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>SSL encrepted file transfer ?</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Secure sharing ?</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Data backed up on multiple servers, with 99.99999% file stablity</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Simple file upload/download</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Image and Document previews</td>
+								<td></td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Revision history (recover deleted files and restore old versions)</td>
+								<td></td>
+								<td></td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>coming soon(analytics)</td>
+								<td>pay per run</td>
+								<td>2 free per week</td>
+								<td>10 per week</td>
+								<td>unlimited</td>
+							</tr>
+							<tr>
+								<td>Flex storate, add more when needed.</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Store archive data</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>x</td>
+							</tr>
+							<br/>
+							<br/>
+							<tr>
+								<td><b>Lab only</b></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Easily add/remove members</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Centralized billing for entire lab</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Data aggreation (coming soon)?</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Store archive data securely (?)</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							
+
+
+						</table>
 		  			</ul>
 			</li>
 			<li class="dropdown">
