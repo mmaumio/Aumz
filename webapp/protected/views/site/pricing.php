b35a94318ed64d5e795ddb56aec2f257e5ecb5ed
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2014-02-01 03:58:48 +0600

    Pricing page html

--- webapp/protected/views/site/pricing.php
+++ webapp/protected/views/site/pricing.php
@@ -6,7 +6,9 @@
 		  		<a data-toggle="dropdown" href="#">Myself</a>
 		  			<ul class="dropdown-menu" role="menu" id="myself-tab" aria-labelledby="dLabel">
 		    			<table border="1" width="900">
-							<p align="center">Individual Plan</p>
+							<tr>
+								<td colspan="5"><span align="center">Individual Plan</span><center>Individual Plan</center></td>
+							</tr>
 							<tr>
 								<td><b>Price</b></td>
 								<td>Free</td>
@@ -98,8 +100,13 @@
 								<td></td>
 								<td>x</td>
 							</tr>
-							<br/>
-							<br/>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
 							<tr>
 								<td><b>Lab only</b></td>
 								<td></td>
@@ -143,14 +150,371 @@
 			</li>
 			<li class="dropdown">
 		  		<a data-toggle="dropdown" href="#">My lab</a>
-		  			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-		    			cpojksjanka 
+		  			<ul class="dropdown-menu" role="menu" id="my-lab" aria-labelledby="dLabel">
+		    			<table border="1" width="500">
+							<tr>
+								<td>Small (1-5)</td>
+								<td>Med (1-15)</td>
+								<td>Large(16+)</td>
+							</tr>
+							<tr>
+								<td>Up to 5 users</td>
+								<td>Up to 15 users</td>
+								<td>More than 16</td>
+							</tr>
+							<tr>
+								<td>$100</td>
+								<td>$200</td>
+								<td>$400</td>
+							</tr>
+							<tr>
+								<td>500GB</td>
+								<td>1TB</td>
+								<td>Begins at 4TB</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>unlimited</td>
+								<td>unlimited</td>
+								<td>unlimited</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>150Gb</td>
+								<td>250GB</td>
+								<td>500GB</td>
+							</tr>
+							
+
+
+						</table>
 		  			</ul>
 			</li>
 			<li class="dropdown">
 		  		<a data-toggle="dropdown" href="#">I’d like to see both</a>
-		  			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-		    			cpojksjanka 
+		  			<ul class="dropdown-menu" role="menu" id="see-both" aria-labelledby="dLabel">
+		    			<table>
+		    				<tr>
+		    					<td>
+		    						<table border="1" width="900">
+		    			    <tr>
+								<td colspan="5"><center>Individual Plan</center></td>
+							</tr>
+							<tr>
+								<td><b>Price</b></td>
+								<td>Free</td>
+								<td>5$ a month</td>
+								<td>20$ a month</td>
+								<td>50$ a month</td>
+							</tr>
+							<tr>
+								<td><b>Base features</b></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Storage</td>
+								<td>2GB</td>
+								<td>10GB</td>
+								<td>2GB</td>
+								<td>100GB</td>
+							</tr>
+							<tr>
+								<td>AES-256 bit  server side encryption ?</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>SSL encrepted file transfer ?</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Secure sharing ?</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Data backed up on multiple servers, with 99.99999% file stablity</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Simple file upload/download</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Image and Document previews</td>
+								<td></td>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Revision history (recover deleted files and restore old versions)</td>
+								<td></td>
+								<td></td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>coming soon(analytics)</td>
+								<td>pay per run</td>
+								<td>2 free per week</td>
+								<td>10 per week</td>
+								<td>unlimited</td>
+							</tr>
+							<tr>
+								<td>Flex storate, add more when needed.</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Store archive data</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td><b>Lab only</b></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Easily add/remove members</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Centralized billing for entire lab</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Data aggreation (coming soon)?</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Store archive data securely (?)</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							
+
+
+						</table>
+		    					</td>
+		    					<td>
+		    						<table border="1" width="400">
+							<tr>
+								<td>Small (1-5)</td>
+								<td>Med (1-15)</td>
+								<td>Large(16+)</td>
+							</tr>
+							<tr>
+								<td>Up to 5 users</td>
+								<td>Up to 15 users</td>
+								<td>More than 16</td>
+							</tr>
+							<tr>
+								<td>$100</td>
+								<td>$200</td>
+								<td>$400</td>
+							</tr>
+							<tr>
+								<td>500GB</td>
+								<td>1TB</td>
+								<td>Begins at 4TB</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>unlimited</td>
+								<td>unlimited</td>
+								<td>unlimited</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td height="30"></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>x</td>
+								<td>x</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>150Gb</td>
+								<td>250GB</td>
+								<td>500GB</td>
+							</tr>
+							
+
+
+						</table>
+		    					</td>
+		    				</tr>
+		    			</table>
 		  			</ul>
 			</li>
 		</ul>
